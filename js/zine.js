5359c370284aaad1a777bbbf2f98b3a667b6d066
gracegalan <grace.galan.e@gmail.com>
2023-12-06 10:19:38 -0800

    trying to make it woooorrrk

--- js/zine.js
+++ js/zine.js
@@ -32,11 +32,19 @@ let numOfPapers = 3;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
-
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translate(180px)";
 }
 
-function closeBook() {
-
+function closeBook(isAtBeginning) {
+    if(isAtBeginning) {
+        book.style.transform = "translateX(0%)"; 
+    } else {
+        book.style.transform = "translateX(100%)"; 
+    }
+    prevBtn.style.transform = "translate(0px)";
+    nextBtn.style.transform = "translateX(0px)";
 }
 
 function goNextPage() {
@@ -45,13 +53,15 @@ function goNextPage() {
             case 1: 
                 openBook();
                 paper1.classList.add("flipped");
-                paper1.style.zIndex = 1
+                paper1.style.zIndex = 1;
                 break;
             case 2: 
                 paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
                 break;
             case 3: 
                 paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
                 closeBook();
                 break;
             default:
@@ -64,5 +74,26 @@ function goNextPage() {
 }
 
 function goPrevPage () {
-
+    if(currentLocation > 1) {
+        switch(currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 3;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 3;
+                break;
+            default:
+                throw new Error("unknown state");
+        }
+    currentLocation--;
+    }
 }
+
